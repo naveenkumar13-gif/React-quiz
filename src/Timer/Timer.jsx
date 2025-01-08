1176711147f30ad3d859c19d75c6468e5316e500
naveenkumar13-gif <naveenkumarofficial1316@gmail.com>
2025-01-08 09:21:47 +0530

    Refactor Timer component for improved readability and consistency

--- src/Timer/Timer.jsx
+++ src/Timer/Timer.jsx
@@ -1,22 +1,25 @@
-import { type } from '@testing-library/user-event/dist/type';
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-const Timer = ({dispatch,secondsRemaining}) => {
-    const mins = Math.floor(secondsRemaining/60)
-    const seconds = secondsRemaining % 60
-    useEffect(function(){
- const id = setInterval(function(){
-dispatch({type:'tick'})
-},1000)
-return()=> clearInterval(id)
-    },[dispatch])
-    return (
-        <div className='timer'>
-           {mins < 10  && '0'}{mins}:
-           {seconds <10  && '0'}{seconds}
-        </div>
-    );
-}
-    
+const Timer = ({ dispatch, secondsRemaining }) => {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+      return () => clearInterval(id);
+    },
+    [dispatch]
+  );
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+};
 
 export default Timer;
+
